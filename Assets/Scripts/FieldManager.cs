f0b8016f2d2d6f43eba090b98322378fd86dde2e
Karol <karol0602@gmail.com>
2018-11-13 23:23:50 +0100

    Dodany manager kolejki - po kliknięciu na kolejkę nowy pionek pojawi się automatycznie na pierwszym wolnym miejscu

--- Assets/Scripts/FieldManager.cs
+++ Assets/Scripts/FieldManager.cs
@@ -2,37 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldManager : MonoBehaviour {
+public class FieldManager : MonoBehaviour
+{
 
     //public PawnManager pawnManager;
     public GameManager gameManager;
-    bool isTaken;
+    public bool isTaken { get; private set; }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         gameObject.GetComponent<Renderer>().enabled = false;
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         isTaken = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnMouseEnter()
-    {
-        gameObject.GetComponent<Renderer>().enabled = true;
     }
 
-    private void OnMouseExit()
+    // Update is called once per frame
+    void Update()
     {
-        gameObject.GetComponent<Renderer>().enabled = false;
+
     }
 
-    private void OnMouseDown()
+    public void PutPawn()
     {
-        if (!isTaken) {
+        if (!isTaken)
+        {
             gameManager.PutPawn(gameObject);
             isTaken = true;
         }
---
+++ Assets/Scripts/QueueManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueueManager : MonoBehaviour {
+
+    public GameManager gameManager;
+    bool isFull;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        isFull = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<Renderer>().enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        FieldManager[] children = gameObject.GetComponentsInChildren<FieldManager>();
+        foreach (FieldManager child in children)
+        {
+            if (!child.isTaken)
+            {
+                child.PutPawn();
+                break;
+            }
+        }
+    }
+}
